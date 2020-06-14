35624e3187fd86e87639d148cb13f6ec8998405a
chipueatfast <15520604@gm.uit.edu.vn>
2020-06-14 16:35:13 +0700

    first init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+function App() {
+    return (
+        <SafeAreaView>
+            <Text>
+                Write to forget
+            </Text>
+        </SafeAreaView>
+    );
+}
+
+export default App;
