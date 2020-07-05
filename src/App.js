8cbbb5c6e2b54c59fd14df40bbb7e35a1d9fddbd
Phat Pham <phat.pham@nfq.asia>
2020-07-05 17:10:48 +0700

    pop up rate without asking

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import 'react-native-gesture-handler';
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, BackHandler} from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import Rate, { AndroidMarket } from 'react-native-rate';
 import SplashScreen from 'react-native-splash-screen';
 import TypePage from '~/container/TypePage/index';
 import StatefulWtf from '~/container/StatefulWtf';
@@ -20,6 +21,21 @@ function App() {
     const [modalVisible, setModalVisible] = useState(false);
     useEffect(() => {
         SplashScreen.hide();
+        const backHandler = BackHandler.addEventListener(
+            'hardwareBackPress',
+            () => {
+                const options = {
+                    GooglePackageName:'com.writetf.writetoforget',
+                    preferredAndroidMarket: AndroidMarket.Google,
+                    preferInApp:false,
+                    openAppStoreIfInAppFails:true,
+                    fallbackPlatformURL: 'https://play.google.com/store/apps/details?id=com.writetf.writetoforget',
+                };
+                Rate.rate(options);
+            }
+          );
+
+        return () => backHandler.remove();
     }, []);
 
     return (
